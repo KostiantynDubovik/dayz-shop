5a32bbf1f26f236e223f302d78a77ea7f3be13b1
Kostiantyn Dubovik <kostiantyn.dubovik@gmail.com>
2022-08-19 18:10:35 +0300

    reollback run sh

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -3,9 +3,9 @@ curl --request GET -sL -o nul\
 			--max-time 30 \
 			--url 'http://root.dayz-shop.com/shutdown'
 cd ../react || return
-git pull --rebase
+git pull
 npm run build
 cd ../../../
-git pull --rebase
+git pull
 ./gradlew clean
 ./gradlew bootRun
