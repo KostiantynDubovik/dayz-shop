0943b81558d0ccc66d4c24caeddb72847ac444c9
Kostiantyn Dubovik <kostiantyn.dubovik@gmail.com>
2022-12-02 19:19:45 +0200

    add get order

--- src/main/java/com/dayz/shop/controllers/OrderController.java
+++ src/main/java/com/dayz/shop/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.dayz.shop.controllers;
 
 import com.dayz.shop.jpa.entities.*;
+import com.dayz.shop.repository.OrderRepository;
 import com.dayz.shop.service.OrderService;
 import com.dayz.shop.utils.OrderUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.openid.OpenIDAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ServerErrorException;
+
+import javax.ws.rs.PathParam;
 
 @RestController
 @RequestMapping("/api/order")
@@ -14,17 +18,30 @@ public class OrderController {
 
 	private final OrderService orderService;
 	private final OrderUtils orderUtils;
+	private final OrderRepository orderRepository;
 
 	@Autowired
-	public OrderController(OrderService orderService, OrderUtils orderUtils) {
+	public OrderController(OrderService orderService, OrderUtils orderUtils, OrderRepository orderRepository) {
 		this.orderService = orderService;
 		this.orderUtils = orderUtils;
+		this.orderRepository = orderRepository;
 	}
 
 	@GetMapping
 	@PreAuthorize("hasAuthority('STORE_READ')")
 	public Order getOrder(@RequestAttribute Store store) {
 		return orderUtils.getCurrentOrder(store);
+	}
+
+	@GetMapping("{orderId}")
+	@PreAuthorize("hasAuthority('STORE_READ')")
+	public Order getOrderById(@PathVariable("orderId") Long orderId, OpenIDAuthenticationToken principal) {
+		Order result = null;
+		Order fromRepo = orderRepository.getById(orderId);
+		if(fromRepo != null && fromRepo.getUser().equals(principal.getPrincipal())) {
+			result = fromRepo;
+		}
+		return result;
 	}
 
 	@PostMapping("add/{item}")
