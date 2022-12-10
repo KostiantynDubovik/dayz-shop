35a5d9c7ce6f0811f3da1e6a584e9093eb19b8f6
Kostiantyn Dubovik <kostiantyn.dubovik@gmail.com>
2022-12-10 09:41:09 +0200

    add configs to restart remote

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -7,7 +7,7 @@ case $1 in
 esac
 curl --request GET -sL -o nul\
 			--max-time 30 \
-			--url 'http://root.dayz-shop.com/shutdown'
+			--url 'https://localhost/shutdown'
 cd ../react || return
 git pull
 npm run build
