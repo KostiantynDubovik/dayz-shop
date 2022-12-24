485f44940102ccbf6a23ee74a3c1eb3caabee292
Kostiantyn Dubovik <kostiantyn.dubovik@gmail.com>
2022-12-24 22:57:25 +0200

    add default sorting by sequnce

--- src/main/java/com/dayz/shop/controllers/ItemController.java
+++ src/main/java/com/dayz/shop/controllers/ItemController.java
@@ -36,7 +36,7 @@ public class ItemController {
 	@GetMapping("{categoryName}/{page}")
 	public Page<Item> getByCategory(@PathVariable("categoryName") String categoryName,
 	                                @PathVariable int page,
-	                                @RequestParam(defaultValue = "name") String sortBy,
+	                                @RequestParam(defaultValue = "sequence") String sortBy,
 	                                @RequestParam(defaultValue = "100") int pageSize,
 	                                @RequestAttribute Store store) {
 		Pageable pageable = PageRequest.of(page > 0 ? page - 1 : 0, pageSize < 3 ? 3 : pageSize, Sort.by(sortBy));
