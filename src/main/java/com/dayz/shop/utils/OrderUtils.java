8c7da66c52f16afb4ce057281b56813f0f2d5c1c
Kostiantyn Dubovik <kostiantyn.dubovik@gmail.com>
2023-01-10 18:42:46 +0200

    return m_code

--- src/main/java/com/dayz/shop/utils/OrderUtils.java
+++ src/main/java/com/dayz/shop/utils/OrderUtils.java
@@ -68,7 +68,7 @@ public class OrderUtils {
 		OrderItem orderItem = new OrderItem();
 		orderItem.setUser(user);
 		orderItem.setOrder(order);
-		orderItem.setCode(String.valueOf(orderItem.getId()));
+		orderItem.setCode(UUID.randomUUID().toString());
 		orderItem.setServer(order.getServer());
 		orderItem.setStatus(OrderStatus.PENDING);
 		orderItem.setReceived(false);
