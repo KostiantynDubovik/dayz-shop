97d386075df3a582320d5fabbc580692a55dcbfe
Kostiantyn Dubovik <kostiantyn.dubovik@gmail.com>
2023-02-12 13:43:13 +0200

    add retry for clearing service

--- src/main/java/com/dayz/shop/service/ClearServices.java
+++ src/main/java/com/dayz/shop/service/ClearServices.java
@@ -16,9 +16,11 @@ import java.util.List;
 
 @Service
 public class ClearServices {
+	private static final int RETRY_COUNT = 3;
 
 	private final UserServiceRepository userServiceRepository;
 	private final SendToServerService sendToServerService;
+
 
 	@Autowired
 	public ClearServices(UserServiceRepository userServiceRepository, SendToServerService sendToServerService) {
@@ -30,21 +32,29 @@ public class ClearServices {
 	public void clearAll() throws JSchException, SftpException, IOException {
 		List<UserService> userServices = userServiceRepository.findAllByEndDateIsBefore(LocalDateTime.now());
 		for (UserService userService : userServices) {
-			clear(userService);
+			clear(userService, 1);
 		}
-		userServiceRepository.deleteAll(userServices);
 	}
 
-	public void clear(UserService userService) throws JSchException, SftpException, IOException {
-		Order order = userService.getOrder();
-		ItemType itemType = userService.getItemType();
-		String steamId = order.getUser().getSteamId();
-		switch (itemType) {
-			case VIP:
-				sendToServerService.vip(order, steamId, false);
-				break;
-			case SET:
-				sendToServerService.set(order, steamId, false);
+	public void clear(UserService userService, int retryNumber) throws JSchException, SftpException, IOException {
+		try {
+			Order order = userService.getOrder();
+			ItemType itemType = userService.getItemType();
+			String steamId = order.getUser().getSteamId();
+			switch (itemType) {
+				case VIP:
+					sendToServerService.vip(order, steamId, false);
+					break;
+				case SET:
+					sendToServerService.set(order, steamId, false);
+			}
+			userServiceRepository.delete(userService);
+		} catch (JSchException | SftpException | IOException e) {
+			if (retryNumber < RETRY_COUNT) {
+				clear(userService, retryNumber + 1);
+			} else {
+				throw e;
+			}
 		}
 	}
 }
