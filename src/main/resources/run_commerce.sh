f08637dcefd73f4ff24d6f47ebf451b135aa85c9
Kostiantyn Dubovik <kostiantyn.dubovik@gmail.com>
2023-04-03 23:37:17 +0300

    configured for voyage

---
+++ src/main/resources/run_commerce.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+DEBUG=''
+case $1 in
+'d')
+  DEBUG='--debug-jvm'
+  ;;
+esac
+curl --request GET -sL -o nul\
+			--max-time 30 \
+			--url 'https://localhost/shutdown'
+cd ../react/voyage || return
+git pull
+npm run build
+cd ../../../
+git pull
+./gradlew clean
+./gradlew bootRun $DEBUG
